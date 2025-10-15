e60f02a0411ec97fb289b9a415d688d15751aa07
mrdarip <darioelas2005@gmail.com>
2025-10-15 11:04:40 +0200

    update API to use Google GenAI and dotenv for environment variables; add GEMINI_API_KEY to .env.example

--- src/pages/api/generateQueryFromNL.ts
+++ src/pages/api/generateQueryFromNL.ts
@@ -1,7 +1,22 @@
-export const GET: APIRoute = ({ params, request }) => {
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+  const ai = new GoogleGenAI({
+    apiKey: GEMINI_API_KEY,
+  });
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash-001",
+    contents: "Why is the sky blue?",
+  });
+
   return new Response(
     JSON.stringify({
-      sql: "SELECT nombre, edad FROM empleados WHERE edad > 30",
+      sql: response.text,
     })
   );
 };
