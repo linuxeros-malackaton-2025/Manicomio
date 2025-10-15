22efc4159a4bc9c005a55ee2019f5ca8947ced6e
mrdarip <darioelas2005@gmail.com>
2025-10-15 11:41:06 +0200

    implement POST endpoint for generating query titles using LLM

--- src/pages/api/generateTitleForQuery.ts
+++ src/pages/api/generateTitleForQuery.ts
@@ -1,7 +1,46 @@
-export const GET: APIRoute = ({ params, request }) => {
-  return new Response(
-    JSON.stringify({
-      title: "Empleados mayores de 30 años",
-    })
-  );
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const prerender = false;
+
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+const ai = new GoogleGenAI({
+  apiKey: GEMINI_API_KEY,
+});
+
+export const POST: APIRoute = async ({ request }) => {
+  if (request.headers.get("Content-Type") === "application/json") {
+    const body = await request.json();
+    const query = body.query;
+
+    if (!query) {
+      return new Response("No se proporcionó una query", { status: 400 });
+    }
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash-001",
+      contents:
+        "Generate a short and descriptive title for the a table that will contain the results of the next query:\n\n" +
+        query +
+        "\n\n Output format: Just and only the title, no explanation",
+    });
+
+    const AIresponse = response.text?.trim(); //la respuesta de la IA
+
+    if(!AIresponse){
+      return new Response("No se generó una respuesta correctamente", { status: 400 });
+    }
+
+    return new Response(
+      JSON.stringify({
+        title: AIresponse,
+      }),
+      {
+        status: 200,
+      }
+    );
+  }
+  return new Response(null, { status: 400 });
 };
